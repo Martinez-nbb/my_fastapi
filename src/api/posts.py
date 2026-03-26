8ea4a591ea8101ded69a36158f84bf04fb8d636a
Makariy <makariy06@mail.ru>
2026-03-26 19:27:23 +0400

    Добавить обработку domain исключений в posts API

--- src/api/posts.py
+++ src/api/posts.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
 
+from src.core.exceptions.domain_exceptions import (
+    PostNotFoundByIdException,
+    UserNotFoundByIdException,
+)
 from src.domain.post.use_cases.get_post import (
     CreatePostUseCase,
     DeletePostUseCase,
@@ -25,13 +29,25 @@ async def get_posts_list() -> list[PostResponseSchema]:
 @router.get('/get/{post_id}')
 async def get_post(post_id: int) -> PostResponseSchema:
     use_case = GetPostUseCase()
-    return await use_case.execute(post_id=post_id)
+    try:
+        return await use_case.execute(post_id=post_id)
+    except PostNotFoundByIdException as exc:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=exc.get_detail(),
+        )
 
 
-@router.post('/create')
+@router.post('/create', status_code=status.HTTP_201_CREATED)
 async def create_post(post: PostCreateSchema) -> PostResponseSchema:
     use_case = CreatePostUseCase()
-    return await use_case.execute(data=post)
+    try:
+        return await use_case.execute(data=post)
+    except UserNotFoundByIdException as exc:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=exc.get_detail(),
+        )
 
 
 @router.put('/update/{post_id}')
@@ -40,14 +56,26 @@ async def update_post(
     post: PostUpdateSchema,
 ) -> PostResponseSchema:
     use_case = UpdatePostUseCase()
-    return await use_case.execute(
-        post_id=post_id,
-        data=post,
-    )
+    try:
+        return await use_case.execute(
+            post_id=post_id,
+            data=post,
+        )
+    except PostNotFoundByIdException as exc:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=exc.get_detail(),
+        )
 
 
 @router.delete('/delete/{post_id}')
 async def delete_post(post_id: int) -> dict:
     use_case = DeletePostUseCase()
-    await use_case.execute(post_id=post_id)
+    try:
+        await use_case.execute(post_id=post_id)
+    except PostNotFoundByIdException as exc:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=exc.get_detail(),
+        )
     return {'message': 'Публикация успешно удалена'}
